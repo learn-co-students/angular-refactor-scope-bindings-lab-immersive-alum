69dbdd7686486cd1f507d8326721b234d47847ed
AllA <allagorik2901@gmail.com>
2017-12-07 15:40:47 -0500

    Done.

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -1,28 +1,32 @@
 function ContactCard() {
-	return {
-		scope: {
-			name: '=',
-			email: '=',
-			phone: '=',
-			username: '='
-		},
-		template: [
-			'<div>',
-				'<h4>Contact Card</h4>',
-				'<label>Name:</label>',
-				'{{ name }}',
-				'<label>Email:</label>',
-				'{{ email }}',
-				'<label>Phone:</label>',
-				'{{ phone }}',
-				'<label>Username:</label>',
-				'<span class="username">{{ username }}</span>',
-			'</div>'
-		].join(''),
-		restrict: 'E'
-	};
-}
-
-angular
-	.module('app')
-	.directive('contactCard', ContactCard);
+    return {
+    scope: {},
+      template: [
+        '<div>',
+          '<h4>Contact Card</h4>',
+          '<label>Name:</label>',
+        '{{ ctrl.name }}',
+          '<label>Email:</label>',
+        '{{ ctrl.email }}',
+          '<label>Phone:</label>',
+        '{{ ctrl.phone }}',
+          '<label>Username:</label>',
+        '<span class="username">{{ ctrl.username }}</span>',
+        '</div>'
+      ].join(''),
+    controller: function () {
+    },
+    controllerAs: 'ctrl',
+    bindToController: {
+      name: '=',
+      email: '=',
+      phone: '=',
+      username: '='
+    },
+      restrict: 'E'
+    }
+  }
+  
+  angular
+    .module('app')
+  .directive('contactCard', ContactCard)
